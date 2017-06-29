89834e0dcafb111100e088f95fdebd55c6721d9d
guochunming <712702384@qq.com>
2017-06-29 19:52:38 +0800

    Add files via upload

---
+++ 郭春明day4/index.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+	alert("这是js代码")
+	var a=0.2;
+	var b=0.1;
+	if(a+b==0.3){
+		alert("a")
+	}else{
+		alert("b")
+	}
+}
---
+++ 郭春明day4/node1.js
@@ -0,0 +1,79 @@
+var fs=require("fs");
+var http=require("http");
+http.createServer(function(a,b){
+	var aa=a.url;
+	switch (aa){
+		case "/":fs.readFile("index.html","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"content":"text/html;"})
+				b.end(data)
+			}
+		})
+		case "/a1":fs.readFile("index.html","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"content":"text/html;"})
+				b.end(data)
+			}
+		})
+		case "/a2"	:fs.readFile("index2.html","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"content":"text/html;"})
+				b.end(data)
+			}
+			
+		})
+		case "/c3"	:fs.readFile("index3.html","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"content":"text/html;"})
+				b.end(data)
+			}
+			
+		})
+		case "/sss"	:fs.readFile("index.css","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"css": "text/css"})
+				b.end(data)
+			}
+			
+		})
+		case "/js"	:fs.readFile("index.js","utf-8",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"js": "text/javascript"})
+				b.end(data)
+			}
+			
+		})
+		case "/img"	:fs.readFile("a.jpg",function(err,data){
+			if(err){
+				console.log("读取失败了！")
+				console.log(err)
+			}else{
+				b.writeHead(200,{"jpg":"image/jpeg"})
+				b.end(data)
+			}
+			
+		})
+		break;
+		default:
+		b.end("404")
+			break;
+	}
+}).listen(8888)
